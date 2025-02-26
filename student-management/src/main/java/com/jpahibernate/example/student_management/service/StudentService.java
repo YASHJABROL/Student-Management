5e9b6e2855a7b8b74b29086510297b727d1f4f9e
YASHJABROL <yashabrol99@gmail.com>
2025-02-26 21:28:41 +0530

    implementing find,delete and update(put and patch both) APIs.

--- student-management/src/main/java/com/jpahibernate/example/student_management/service/StudentService.java
+++ student-management/src/main/java/com/jpahibernate/example/student_management/service/StudentService.java
@@ -5,6 +5,9 @@ import com.jpahibernate.example.student_management.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class StudentService {
 
@@ -15,4 +18,48 @@ public class StudentService {
         studentRepository.save(student);
         return "Student saved successfully";
     }
+     public Student findStudentById(int id){
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        return studentOptional.get();
+     }
+     public List<Student> findAllStudents()
+     {
+         List<Student> studentList = studentRepository.findAll();
+         return studentList;
+     }
+
+     public String deleteStudentById(int id)
+     {
+         studentRepository.deleteById(id);
+         return "Student with id : " +id+ "is deleted successfully";
+     }
+
+     public String updateStudentByPut(int studentId,Student studentRequest)
+     {
+         Student studentFromDb = findStudentById(studentId);
+         if(studentFromDb!=null)
+         {
+             studentRepository.save(studentRequest);
+             return "Student update successfully";
+         }
+         else
+         {
+             return "Cannot find student to update";
+         }
+     }
+
+     public String updateStudentMobileByPatch(int studentId,String mobile)
+     {
+         Student studentFromDb = findStudentById(studentId);
+         if(studentFromDb!=null)
+         {
+             studentFromDb.setMobile(mobile);
+             studentRepository.save(studentFromDb);
+             return "Student mobile number updated successfully";
+         }
+         else
+         {
+             return "Cannot find student to update";
+         }
+     }
 }
